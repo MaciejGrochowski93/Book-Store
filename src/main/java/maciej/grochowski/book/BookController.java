c2313b942fe5d51ffb9bb475a82e4e22ca873314
Maciej <maciejgrochowski93@o2.pl>
2021-04-21 17:06:31 +0200

    changing Controller and Service methods into ResponseEntities

--- src/main/java/maciej/grochowski/book/BookController.java
+++ src/main/java/maciej/grochowski/book/BookController.java
@@ -1,12 +1,10 @@
 package maciej.grochowski.book;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/books")
@@ -16,56 +14,48 @@ public class BookController {
     private BookService bookService;
 
     @RequestMapping
-    public List<Book> getAllBooks() {
+    public ResponseEntity<List<Book>> getAllBooks() {
         return bookService.getAllBooks();
     }
 
-    @RequestMapping(value = "/booksAvailable")
-    public ResponseEntity<List<Book>> getSomeBooks() {
-        List<Book> booksList = bookService.getAllBooks();
-        if (booksList.size() < 1) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
-        return ResponseEntity.of(Optional.of(booksList));
-    }
 
     @RequestMapping(method = RequestMethod.PUT, value = "/{id}/sell")
-    public void sellBook(@PathVariable int id) {
-        bookService.sellBook(id);
+    public ResponseEntity<Void> sellBook(@PathVariable int id) {
+        return bookService.sellBook(id);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/{id}")
-    public Book getBookById(@PathVariable int id) {
+    public ResponseEntity<Book> getBookById(@PathVariable int id) {
         return bookService.getBookById(id);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/category/{category}")
-    public List<Book> getBookByCategory(@PathVariable String category) {
+    public ResponseEntity<List<Book>> getBookByCategory(@PathVariable String category) {
         return bookService.getBookByCategory(category);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/author/{author}")
-    public List<Book> getBookByAuthor(@PathVariable String author) {
+    public ResponseEntity<List<Book>> getBookByAuthor(@PathVariable String author) {
         return bookService.getBookByAuthor(author);
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/title/{title}")
-    public List<Book> getBookByTitle(@PathVariable String title) {
+    public ResponseEntity<List<Book>> getBookByTitle(@PathVariable String title) {
         return bookService.getBookByTitle(title);
     }
 
     @RequestMapping(method = RequestMethod.POST)
-    public void addBook(@RequestBody Book book) {
-        bookService.addBook(book);
+    public ResponseEntity<Void> addBook(@RequestBody Book book) {
+        return bookService.addBook(book);
     }
 
     @RequestMapping(method = RequestMethod.PUT, value = "/{id}")
-    public void updateBook(@RequestBody Book book, @PathVariable Integer id) {
-        bookService.updateBook(book, id);
+    public ResponseEntity<Void> updateBook(@RequestBody Book book, @PathVariable Integer id) {
+        return bookService.updateBook(book, id);
     }
 
     @RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
-    public void deleteBook(@PathVariable int id) {
-        bookService.deleteBook(id);
+    public ResponseEntity<Void> deleteBook(@PathVariable int id) {
+        return bookService.deleteBook(id);
     }
 }
--- src/main/java/maciej/grochowski/book/BookService.java
+++ src/main/java/maciej/grochowski/book/BookService.java
@@ -1,6 +1,9 @@
 package maciej.grochowski.book;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -13,29 +16,46 @@ public class BookService {
     @Autowired
     private BookRepository bookRepository;
 
-    public List<Book> getAllBooks() {
-        List<Book> booksList = new ArrayList<>();
-        bookRepository.findAll().forEach(booksList::add);
-        return booksList;
+    public ResponseEntity<List<Book>> getAllBooks() {
+        List<Book> bookList = new ArrayList<>();
+        HttpHeaders header = new HttpHeaders();
+        header.set("desc", "Getting all available books");
+        bookRepository.findAll().forEach(bookList::add);
+        return new ResponseEntity<>(bookList, header, HttpStatus.OK);
     }
 
-    public Book getBookById(int id) {
-        return bookRepository.findById(id).orElse(null);
+    public ResponseEntity<Book> getBookById(int id) {
+        Optional<Book> book = bookRepository.findById(id);
+        HttpHeaders header = new HttpHeaders();
+        header.set("desc", "Returns a single book by the given ID");
+        return ResponseEntity.status(HttpStatus.OK).headers(header).body(book.get());
+
     }
 
-    public void addBook(Book book) {
+    public ResponseEntity<Void> addBook(Book book) {
+        HttpHeaders header = new HttpHeaders();
+        header.add("desc", "Adds a new Book");
         bookRepository.save(book);
+        return ResponseEntity.status(HttpStatus.OK).headers(header).build();
     }
 
-    public void updateBook(Book book, int id) {
+    public ResponseEntity <Void> updateBook(Book book, int id) {
+        HttpHeaders header = new HttpHeaders();
+        header.add("desc", "Changes attributes of the book.");
         bookRepository.save(book);
+        return ResponseEntity.status(HttpStatus.OK).headers(header).build();
     }
 
-    public void deleteBook(int id) {
+    public ResponseEntity <Void> deleteBook(int id) {
+        HttpHeaders header = new HttpHeaders();
+        header.add("desc", "Removes a book from the shop's offer.");
         bookRepository.deleteById(id);
+        return ResponseEntity.status(HttpStatus.OK).headers(header).build();
     }
 
-    public void sellBook(int id) {
+    public ResponseEntity <Void> sellBook(int id) {
+        HttpHeaders header = new HttpHeaders();
+        header.add("desc", "Removes 1 copy of the particular book.");
         Optional<Book> book = bookRepository.findById(id);
         book.stream().findFirst()
                 .ifPresent(e -> {
@@ -44,30 +64,37 @@ public class BookService {
                         bookRepository.save(e);
                     }
                 });
+        return ResponseEntity.status(HttpStatus.OK).headers(header).build();
     }
 
-    public List<Book> getBookByCategory(String category) {
+    public ResponseEntity <List<Book>> getBookByCategory(String category) {
         List<Book> bookList = new ArrayList<>();
+        HttpHeaders header = new HttpHeaders();
+        header.add("desc", "Returns a list of books of the given category.");
         bookRepository.findAll().stream()
                 .filter(e -> e.getCategory().toLowerCase().equals(category.toLowerCase()))
                 .forEach(bookList::add);
-        return bookList;
+        return ResponseEntity.status(HttpStatus.OK).headers(header).body(bookList);
     }
 
-    public List<Book> getBookByAuthor(String author) {
+    public ResponseEntity <List<Book>> getBookByAuthor(String author) {
         List<Book> bookList = new ArrayList<>();
+        HttpHeaders header = new HttpHeaders();
+        header.add("desc", "Returns a list of books of the given author.");
         bookRepository.findAll().stream()
                 .filter(e -> e.getAuthor().toLowerCase().contains(author.toLowerCase()))
                 .forEach(bookList::add);
-        return bookList;
+        return ResponseEntity.status(HttpStatus.OK).headers(header).body(bookList);
     }
 
-    public List<Book> getBookByTitle(String title) {
+    public ResponseEntity<List<Book>> getBookByTitle(String title) {
         List<Book> bookList = new ArrayList<>();
+        HttpHeaders header = new HttpHeaders();
+        header.add("desc", "Returns a list of books containing the given word.");
         bookRepository.findAll().stream()
                 .filter(e -> e.getTitle().toLowerCase().contains(title.toLowerCase()))
                 .forEach(bookList::add);
-        return bookList;
+        return ResponseEntity.status(HttpStatus.OK).headers(header).body(bookList);
     }
 }
 
